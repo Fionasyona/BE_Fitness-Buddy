a018df75512bce812ea4314c12d9cdecd24cb34b
Fiona Wambua <fionawambua20@gmail.com>
2025-08-23 11:27:09 +0300

    creation of class profile in api/model.py

---
+++ fitness_tracker/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True, null=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    weight = models.FloatField(blank=True, null=True)  # in kg
+    height = models.FloatField(blank=True, null=True)  # in cm
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
